df7d80c657eccc7c6838857197c0d47f78ad7d05
gabrieleeccher <gabriele.eccher@gmail.com>
2016-03-08 18:39:17 +0100

    20160308@Gabriele Formule Loganathan

--- smt/utils.py
+++ smt/utils.py
@@ -32,9 +32,67 @@ def cob_step_2(z_ref, ref_stratus,sigma_v,z_wt, z_tun, gamma_muck):
     ka = (1.- math.sin(phi))/(1+math.sin(phi))
     sigma_ha_eff = sigma_v_eff * ka - 2.*ci*math.sqrt(ka)
     pCob = sigma_ha_eff + p_wt + 20.0 + (z_tun-z_ref)*gamma_muck
-    return pCob        
-        
-        
+    return pCob
+
+# cedimento a profondita' z dalla superficie secondo laganathan 2011
+# eps0 = volume perso (adimensionale)
+# R = raggio di scavo in metri
+# H = profondita' asse tunnel dalla superficie
+# nu = coefficiente di poisson mediato dall'asse galleria alla superficie
+# beta = 45° + coeff. di attrito/2 mediato dall'asse alla superficie. Per argille coeff attrito = 0
+# x distanza planimetrica ortogonale del punto di misura dall'asse
+# z profondita' del punto di misura dalla superficie
+def uz_laganathan(eps0, R, H, nu, beta, x, z):
+    uz = eps0*R**2*((H - z)/(x**2 + (-H + z)**2) - (2*z*(x**2 - (H + z)**2))/(x**2 + (H + z)**2)**2 + \
+    ((3. - 4.*nu)*(H + z))/(x**2 + (H + z)**2))*math.exp((-0.69*z**2)/H**2 - (1.38*x**2)/(R + H*math.atan(beta))**2)
+    return uz
+
+# spostamento orizzontale a profondita' z dalla superficie secondo laganathan 2011
+# eps0 = volume perso (adimensionale)
+# R = raggio di scavo in metri
+# H = profondita' asse tunnel dalla superficie
+# nu = coefficiente di poisson mediato dall'asse galleria alla superficie
+# beta = 45° + coeff. di attrito/2 mediato dall'asse alla superficie. Per argille coeff attrito = 0
+# x distanza planimetrica ortogonale del punto di misura dall'asse
+# z profondita' del punto di misura dalla superficie
+def ux_laganathan(eps0, R, H, nu, beta, x, z):
+    ux = -(eps0*R**2*x*(1./(x**2 + (H - z)**2) - (4.*z*(H + z))/(x**2 + (H + z)**2)**2 + \
+    (3. - 4.*nu)/(x**2 + (H + z)**2))*math.exp((-0.69*z**2)/H**2 - (1.38*x**2)/(R + H*math.atan(beta))**2))
+    return ux
+
+
+# rotazione o inclinazione superfice (beta) a profondita' z dalla superficie secondo laganathan 2011
+# eps0 = volume perso (adimensionale)
+# R = raggio di scavo in metri
+# H = profondita' asse tunnel dalla superficie
+# nu = coefficiente di poisson mediato dall'asse galleria alla superficie
+# beta = 45° + coeff. di attrito/2 mediato dall'asse alla superficie. Per argille coeff attrito = 0
+# x distanza planimetrica ortogonale del punto di misura dall'asse
+# z profondita' del punto di misura dalla superficie
+def d_uz_dx_laganathan(eps0, R, H, nu, beta, x, z):
+    duz=eps0*R**2*((-2.*x*(H - z))/(x**2 + (-H + z)**2)**2 + (8.*x*z*(x**2 - (H + z)**2))/(x**2 + (H + z)**2)**3 - \
+    (4.*x*z)/(x**2 + (H + z)**2)**2 - (2*(3. - 4.*nu)*x*(H + z))/(x**2 + (H + z)**2)**2)*math.exp((-0.69*z**2)/H**2 - \
+    (1.38*x**2)/(R + H*math.atan(beta))**2) - (2.76*eps0*R**2*x*((H - z)/(x**2 + (-H + z)**2) - (2.*z*(x**2 - \
+    (H + z)**2))/(x**2 + (H + z)**2)**2 + ((3. - 4.*nu)*(H + z))/(x**2 + (H + z)**2))*math.exp((-0.69*z**2)/H**2 - \
+    (1.38*x**2)/(R + H*math.atan(beta))**2))/(R + H*math.atan(beta))**2
+    return duz
+
+
+# espilon orizzontale a profondita' z dalla superficie secondo laganathan 2011
+# eps0 = volume perso (adimensionale)
+# R = raggio di scavo in metri
+# H = profondita' asse tunnel dalla superficie
+# nu = coefficiente di poisson mediato dall'asse galleria alla superficie
+# beta = 45° + coeff. di attrito/2 mediato dall'asse alla superficie. Per argille coeff attrito = 0
+# x distanza planimetrica ortogonale del punto di misura dall'asse
+# z profondita' del punto di misura dalla superficie
+def d_ux_dx_laganathan(eps0, R, H, nu, beta, x, z):
+    dux = -(eps0*R**2*x*((-2*x)/(x**2 + (H - z)**2)**2 + (16*x*z*(H + z))/(x**2 + (H + z)**2)**3 - (2*(3 - 4*nu)*x)/(x**2 + \
+    (H + z)**2)**2)*math.exp((-0.69*z**2)/H**2 - (1.38*x**2)/(R + H*math.atan(beta))**2)) - eps0*R**2*(1/(x**2 + (H - z)**2) - \
+    (4*z*(H + z))/(x**2 + (H + z)**2)**2 + (3 - 4*nu)/(x**2 + (H + z)**2))*math.exp((-0.69*z**2)/H**2 - \
+    (1.38*x**2)/(R + H*math.atan(beta))**2) + (2.76*eps0*R**2*x**2*(1/(x**2 + (H - z)**2) - (4*z*(H + z))/(x**2 + (H + z)**2)**2 + \
+    (3 - 4*nu)/(x**2 + (H + z)**2))*math.exp((-0.69*z**2)/H**2 - (1.38*x**2)/(R + H*math.atan(beta))**2))/(R + H*math.atan(beta))**2
+    return dux
         
 """        
 def latLonToProjection(lat, lon, epsg):
