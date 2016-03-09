cbfb38f843eb83421f843096531f4ea06be0ad26
andreadanzi <andrea@danzi.tn.it>
2016-03-09 21:59:52 +0100

    danzi.tn@20160309 plot parallel

--- smt/plot_data.py
+++ smt/plot_data.py
@@ -16,6 +16,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.mlab import griddata
 from collections import defaultdict
+from shapely.geometry import LineString
 # create main logger
 logger = logging.getLogger('smt_main')
 logger.setLevel(logging.DEBUG)
@@ -35,6 +36,7 @@ database = smtConfig.get('MONGODB','database')
 source_database = smtConfig.get('MONGODB','source_database')
 username = smtConfig.get('MONGODB','username')
 password = smtConfig.get('MONGODB','password')
+# Colori da associare alle aree degli strati di riferimento
 main_colors = {
                 'MC':'#1f77b4',
                 'MCA':'#ff7f0e', 
@@ -48,6 +50,49 @@ main_colors = {
                 'MFL4':'#dbdb8d', 
                 'SO':'#17becf', 
                 'MFL3':'#9edae5'}
+
+def mid_point(p1,p2):
+    x = (p1[0] + p2[0])/2.
+    y = (p1[1] + p2[1])/2.
+    return x,y
+
+def asse2p(p1,p2,x):
+    coef = np.polyfit(p1,p2,1)    
+    m = mid_point(p1,p2)
+    fm = -1/coef[0]
+    fb = m[0]/coef[0]+m[1]
+    return fm*x+fb
+
+def pfromdistance(p1,p2,d):
+    coef = np.polyfit(p1,p2,1)    
+    m = mid_point(p1,p2)
+    fb = m[0]/coef[0]+m[1]
+    fm = -1/coef[0]
+    a= 1+fm**2
+    b=-2*fm*fb-2*fm*m[1]-2*m[0]
+    c=m[0]**2+m[1]**2+fb**2-2*fb*m[1]-d**2
+    coeff = [a, b, c]
+    res_x = np.roots(coeff)
+    res_y = asse2p(p1,p2,res_x)
+    return (res_x[0],res_y[0]),(res_x[1],res_y[1])
+        
+def plot_line( ob, color="green"):
+    parts = hasattr(ob, 'geoms') and ob or [ob]
+    for part in parts:
+        x, y = part.xy
+        plt.plot(x, y,'o', color=color, zorder=1) 
+        #plt.plot(x, y, color=color, linewidth=2, solid_capstyle='round', zorder=1)    
+
+
+def plot_coords( x, y, color='#999999', zorder=1):
+    plt.plot(x, y, 'o', color=color, zorder=zorder)
+
+
+def outputFigure(sDiagramsFolderPath, sFilename, format="svg"):
+    imagefname=os.path.join(sDiagramsFolderPath,sFilename)
+    if os.path.exists(imagefname):
+        os.remove(imagefname)
+    plt.savefig(imagefname,format=format, bbox_inches='tight', pad_inches=0)
 
 def fillBetweenStrata(a_list):
     currentStrata = None
@@ -77,7 +122,7 @@ def plot_data(bAuthenticate, sPath):
     # connect to MongoDB
     client = MongoClient()
     db = client[database]
-    # DB authentication
+    # DB authentication if required
     if bAuthenticate:
         bLoggedIn = db.authenticate(username,password,source=source_database)
     else:
@@ -106,6 +151,7 @@ def plot_data(bAuthenticate, sPath):
                 pkys =[d['PH']['coordinates'][1] for d in a_list]
                 pkxs =[d['PH']['coordinates'][0] for d in a_list]                
                 # plot
+                fig = plt.figure()
                 plt.title("Profilo")
                 fillBetweenStrata(a_list)
                 plt.plot(pks,phs, linewidth=2,label='Tracciato')
@@ -114,15 +160,29 @@ def plot_data(bAuthenticate, sPath):
                 plt.plot(pks,blowups, label='BLOWUP / 100')
                 plt.axis([min(pks),max(pks),min(phs)-10,max(dems)+10])
                 plt.legend()
+                outputFigure(sPath, "profilo.svg")
+                logger.info("profilo.svg plotted in %s" % sPath)
                 plt.show()
                 # stampa planimetria
                 plt.title("Planimetria") 
-                plt.plot(pkxs,pkys, linewidth=2,label='Tracciato')
+                # plt.plot(pkxs,pkys, linewidth=2,label='Tracciato')
+                line_points = [(x,pkys[i]) for i,x in enumerate(pkxs) ]
+                line = LineString(line_points)
+                print "line ha %d coords" % len(line.coords)
+                offset_left = line.parallel_offset(20, 'left', join_style=2, mitre_limit=20)
+                offset_center = offset_left.parallel_offset(20, 'right', join_style=2, mitre_limit=20)
+                print "offset_left ha %d coords" % len(offset_left.coords)
+                offset_right = line.parallel_offset(20, 'right', join_style=2, mitre_limit=20)
+                print "offset_right ha %d coords" % len(offset_right.coords)
+                plot_line(offset_center)
+                plot_line(offset_left, color="blue")
+                plot_line(offset_right, color="red")
+                x, y = list(line.coords)[0]
+                plot_coords(x, y)
                 plt.axis("equal")
                 plt.legend()
                 plt.show()
-                
-                logger.info("Found %d PKs" % len( a_list ) )
+                logger.info("plot_data terminated!")
     else:
         logger.error("Authentication failed")
         
@@ -150,6 +210,7 @@ def main(argv):
             if not os.path.isdir(sPath):
                 print sSyntax
                 print "Directory %s does not exists!" % sPath
+                sPath = None
                 sys.exit(3)
     if sPath:
         plot_data(bAuthenticate, sPath)
