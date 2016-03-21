a9138fb8ab10ed047f7f2453c74b7784e33e7363
Andrea Ghensi <Andrea Ghensi>
2016-03-21 12:00:56 +0100

    aghensi@20160321 export CSV alignments per geotecnici

---
+++ smt/export_pk_data.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import logging
+import logging.handlers
+import ConfigParser
+import os
+import csv
+import sys
+import getopt
+from pymongo import MongoClient
+
+from alignment_set import AlignmentSet
+from project import Project
+from domain import Domain
+
+# danzi.tn@20160310 plot secondo distanze dinamiche
+# create main logger
+logger = logging.getLogger('smt_main')
+logger.setLevel(logging.DEBUG)
+# create a rotating file handler which logs even debug messages 
+fh = logging.handlers.RotatingFileHandler('export_data.log',maxBytes=5000000, backupCount=5)
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+# reading config file
+sCFGName = 'smt.cfg'
+smtConfig = ConfigParser.RawConfigParser()
+smtConfig.read(sCFGName)
+# setup DB parameter
+host = smtConfig.get('MONGODB','host')
+database = smtConfig.get('MONGODB','database')
+source_database = smtConfig.get('MONGODB','source_database')
+username = smtConfig.get('MONGODB','username')
+password = smtConfig.get('MONGODB','password')
+# Colori da associare alle aree degli strati di riferimento
+
+def export_data_csv(bAuthenticate, sPath):
+    # connect to MongoDB
+    client = MongoClient()
+    db = client[database]
+    # DB authentication if required
+    if bAuthenticate:
+        bLoggedIn = db.authenticate(username,password,source=source_database)
+    else:
+        bLoggedIn = True
+    if bLoggedIn:
+        logger.info("Logged in")
+        pd = db.Project.find_one({"project_code":"MFW001_0-010 Metro Paris-Ligne 15_T2A"})
+        if pd:
+            logger.info("Project %s found" % pd["project_name"])
+            p = Project(db, pd)
+            p.load()
+            found_domains = Domain.find(db, {"project_id": p._id})
+            for dom in found_domains:
+                dm = Domain(db,dom)
+                dm.load()
+                # Example of query
+                asets = db.AlignmentSet.find({"domain_id": dm._id})
+                for aset in asets:
+                    a_set = AlignmentSet(db,aset)
+                    a_set.load()
+                    sCode = a_set.item["code"]
+                    als = db.Alignment.find({"alignment_set_id":a_set._id},{"PK":True, "VOLUME_LOSS":True, "SETTLEMENT_MAX":True, "K_PECK":True, "phi_tun":True, "ci_tun":True}).sort("PK", 1)
+                    a_list = list(als)
+                    header = ["PK","VOLUME_LOSS","SETTLEMENT_MAX","K_PECK","phi_tun","ci_tun"]
+                    rows = []
+                    for d in a_list:
+                        row = [d["PK"], d.get("VOLUME_LOSS",""), d.get("SETTLEMENT_MAX",""),
+                               d.get("K_PECK",""), d.get("phi_tun",""), d.get("ci_tun","")]
+                        rows.append(row)
+                    with open(os.path.join(sPath,sCode + "_export.csv"), 'wb') as out_csvfile:
+                        writer = csv.writer(out_csvfile,delimiter=";")
+                        writer.writerow(header)
+                        writer.writerows(rows)
+                    logger.info("export_data_csv terminated!")
+    else:
+        logger.error("Authentication failed")        
+                
+def main(argv):
+    sPath = None
+    bAuthenticate = False
+    sSyntax = os.path.basename(__file__) +" -p <output folder path> [-a for autentication -h for help]"
+    try:
+        opts, args = getopt.getopt(argv,"hap:",["path="])
+    except getopt.GetoptError:
+        print sSyntax
+        sys.exit(1)
+    if len(opts) < 1:
+        print sSyntax
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print sSyntax
+            sys.exit()
+        elif opt == '-a':
+            bAuthenticate = True
+        elif opt in ("-p", "--path"):
+            sPath = arg
+            if not os.path.isdir(sPath):
+                print sSyntax
+                print "Directory %s does not exists!" % sPath
+                sPath = None
+                sys.exit(3)
+    if sPath:
+        export_data_csv(bAuthenticate, sPath)
+    
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
